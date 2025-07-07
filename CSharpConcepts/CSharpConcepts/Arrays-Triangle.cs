3498033ecda774e452f45352c8cbe006f8d43556
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-07 09:20:16 -0400

    created new array exercise

---
+++ CSharpConcepts/CSharpConcepts/Arrays-Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    public class Arrays_Triangle
+    {
+        public void Triangle()
+        {
+            Console.WriteLine("Enter three sides of a shape to see if it's a triangle");
+
+        }
+
+    }
+}
