de36d6d0ffba9ab7375d661db849c49387da07c9
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-11 10:49:31 -0400

    Add ListBasic method for dynamic integer list handling
    This commit introduces a new static method `ListBasic` in the `Lists` class of the `CSharpConcepts` namespace. The method allows users to input integers into a dynamically sized list and prints the contents of the list. This enhances the functionality for scenarios where the list size is not predetermined.

--- CSharpConcepts/CSharpConcepts/Lists.cs
+++ CSharpConcepts/CSharpConcepts/Lists.cs
@@ -9,7 +9,23 @@ namespace CSharpConcepts
     public class Lists
     {
         //Best if size of list is unknown and good for modifying
+        public static void ListBasic()
+        {
+            List<int> listNumbers = new List<int>(); //how to add{ 1, 2, 4, 5 };
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Enter a number {i + 1}: ");
+                listNumbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
 
+            for (int i = 0; i < listNumbers.Count; i++)
+            {
+                Console.WriteLine(listNumbers[i]);
+
+            }
+
+        }
+        
 
     }
 }
