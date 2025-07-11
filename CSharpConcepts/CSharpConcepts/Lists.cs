0f89612229afbb103dc8562bbbc57999c07cd345
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-11 10:41:34 -0400

    Added Lists

---
+++ CSharpConcepts/CSharpConcepts/Lists.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    public class Lists
+    {
+        //Best if size of list is unknown and good for modifying
+
+
+    }
+}
