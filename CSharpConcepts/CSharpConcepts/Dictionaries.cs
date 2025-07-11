f3e167034d7a02be946fee015f1210c6358f6b1c
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-11 13:25:44 -0400

    Add dictionaryBasic method to Dictionaries class
    Introduced a new static method `dictionaryBasic` in the
    `Dictionaries` class that initializes a `Dictionary<int, string>`
    with three unique key-value pairs: 1 -> "One", 2 -> "Two",
    and 3 -> "Three". This method demonstrates basic dictionary
    initialization in C#.

--- CSharpConcepts/CSharpConcepts/Dictionaries.cs
+++ CSharpConcepts/CSharpConcepts/Dictionaries.cs
@@ -2,6 +2,24 @@
 {
     public class Dictionaries
     {
+        public static void dictionaryBasic()
+        {
+            Dictionary<int, string> dictOne = new Dictionary<int, string>()
+            {
+                //Add via keyvalue pairs and must be unique
+                {1, "One" },
+                {2, "Two" },
+                {3, "Three" }
+            };
+
+
+
+
+
+
+        }
+
+
 
     }
 }
