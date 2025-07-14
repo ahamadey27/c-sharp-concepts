52c9b223ca9b2965934f6a3112b3b06655e77a4a
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-14 12:50:42 -0400

    started array of multiples

---
+++ CSharpConcepts/CSharpConcepts/ArrayOfMultiples.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpConcepts
+{
+    public class ArrayOfMultiples
+    {
+        //Define and initialize two ints (num, length)
+        //(7, 5) => [7, 14, 21, 28, 35]
+        //Create int array with size length
+        //loops through and insert into array
+        //print final array
+        public static void ArrayMult()
+        {
+
+        }
+    }
+}
