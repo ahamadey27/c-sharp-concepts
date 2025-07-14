3ee65c02190597e194a8c53031413575e7d5cd87
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-14 14:43:51 -0400

    Add ArrayMult method to generate and print multiples
    Introduces a new method `ArrayMult` that creates an integer array filled with multiples of 7 based on a specified length. The method populates the array and prints each element to the console.

--- CSharpConcepts/CSharpConcepts/ArrayOfMultiples.cs
+++ CSharpConcepts/CSharpConcepts/ArrayOfMultiples.cs
@@ -9,7 +9,23 @@
         //print final array
         public static void ArrayMult()
         {
+            int num = 7;
+            int length = 5;
+            int[] result = new int[length];
 
+            for (int i = 1; i < result.Length; i++)
+            {
+                result[i - 1] = num * i;
+            }
+
+            foreach (var item in result)
+            {
+                Console.Write($"{item}, ");
+            }
         }
+
+
+
+
     }
 }
