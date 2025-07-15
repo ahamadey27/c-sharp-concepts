e87e6f9f9d523f0bd185771204df560fd7d57a31
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-15 17:08:25 -0400

    Add CalcApp class for basic arithmetic operations
    Introduces the `CalcApp` class in the `CSharpConcepts` namespace with a static method `Calc()`. This method allows users to perform addition, subtraction, multiplication, and division based on their input. It includes input validation for operators and prompts the user until a valid operator is provided.

---
+++ CSharpConcepts/CSharpConcepts/CalcApp.cs
@@ -0,0 +1,46 @@
+﻿namespace CSharpConcepts
+{
+    public class CalcApp
+    {
+        public static void Calc()
+        {
+            bool isValid;
+            char userChar;
+            Console.WriteLine("Enter first int");
+            int intOne = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Enter operator");
+                string userInput = Console.ReadLine();
+                isValid = char.TryParse(userInput, out userChar);
+
+                if (userChar == '+' || userChar == '-' || userChar == '/' || userChar == '*')
+                {
+                    Console.WriteLine("Enter second int");
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid integer");
+                }
+            } while (!isValid);
+
+            int intTwo = Convert.ToInt32(Console.ReadLine());
+
+            int addition = intOne + intTwo;
+            int subrtract = intOne - intTwo;
+            int multiply = intOne * intTwo;
+            int divide = intOne / intTwo;
+
+            switch (userChar)
+            {
+                case '+': Console.WriteLine($"{intOne} + {intTwo} = {addition}"); break;
+                case '-': Console.WriteLine($"{intOne} - {intTwo} = {subrtract}"); break;
+                case '*': Console.WriteLine($"{intOne} * {intTwo} = {multiply}"); break;
+                case '/': Console.WriteLine($"{intOne} / {intTwo} = {divide}"); break;
+            }
+
+
+        }
+    }
+}
