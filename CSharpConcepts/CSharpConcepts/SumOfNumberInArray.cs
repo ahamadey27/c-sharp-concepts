eb92ad57199c4be76ef1c44d7718f22442dfffdc
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-16 12:04:22 -0400

    Add SumOfNumberInArray class for user input array
    Introduces the `SumOfNumberInArray` class in the `CSharpConcepts` namespace.
    The class includes a static method `SumNumArray` that allows users to specify the length of an array and input integer values to populate it.
    Currently, the method does not perform any operations on the array after population.

---
+++ CSharpConcepts/CSharpConcepts/SumOfNumberInArray.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpConcepts
+{
+    public class SumOfNumberInArray
+    {
+        public static void SumNumArray()
+        {
+            Console.Write("How long would you like array to be?");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+            int[] newArray = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write($"Enter array position {i + 1}");
+                newArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int arraySum;
+
+
+
+
+        }
+    }
+}
